abbb1538d4b2e4c626606ef07851e2ea025d1f55
yanmoai6666-star <yanmoai6667@163.com>
2025-12-04 10:37:39 +0800

    style: standardize function comments in calculator and logger

--- pkg/calculator/arithmetic.go
+++ pkg/calculator/arithmetic.go
@@ -1,16 +1,16 @@
 package calculator
 
-// 计算两个整数的平方和
+// SquareSum 计算两个整数的平方和
 func SquareSum(a, b int) int {
 	return a*a + b*b
 }
 
-// 计算两个整数的平方差
+// SquareDifference 计算两个整数的平方差
 func SquareDifference(a, b int) int {
 	return a*a - b*b
 }
 
-// 计算一个数的绝对值
+// Abs 计算一个数的绝对值
 func Abs(x int) int {
 	if x < 0 {
 		return -x
@@ -18,12 +18,12 @@ func Abs(x int) int {
 	return x
 }
 
-// 计算一个数的三次方
+// Cube 计算一个数的三次方
 func Cube(x int) int {
 	return x * x * x
 }
 
-// 计算两个数的最大值
+// Max 计算两个数的最大值
 func Max(a, b int) int {
 	if a > b {
 		return a
@@ -31,7 +31,7 @@ func Max(a, b int) int {
 	return b
 }
 
-// 计算两个数的最小值
+// Min 计算两个数的最小值
 func Min(a, b int) int {
 	if a < b {
 		return a
